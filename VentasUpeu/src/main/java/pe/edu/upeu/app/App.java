7bcb16ce0adff95d09ca71208a285978fc97dbf3
danielchp <eddypineda135@gmail.com>
2020-12-16 03:37:35 +0000

    proyec

---
+++ VentasUpeu/src/main/java/pe/edu/upeu/app/App.java
@@ -0,0 +1,131 @@
+package pe.edu.upeu.app;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.edu.upeu.core.Reportes;
+import pe.edu.upeu.dao.ClientesDao;
+import pe.edu.upeu.dao.ProductosDao;
+import pe.edu.upeu.dao.VentasDao;
+import pe.edu.upeu.gui.MainGUI;
+import pe.edu.upeu.utils.LeerArchivo;
+import pe.edu.upeu.utils.LeerTeclado;
+import pe.edu.upeu.utils.UtilsX;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+
+    static LeerTeclado teclado=new LeerTeclado();
+    static Console cons=System.console();
+
+    static Reportes rep=new Reportes(); 
+    static UtilsX util=new UtilsX();
+    static public void crearArchivo(String nombre){
+           LeerArchivo la=new LeerArchivo(nombre);
+            try {
+                List list = new ArrayList<String>();
+                list.add("P001	Papa fritas	porcion	2.5	30");
+                list.add("P002	Gaseosa	litros	3	48");
+                la.escribir(list);    
+                
+                List listaLectura=la.leer();
+                System.out.println("Contenido de Productos");
+                System.out.println(listaLectura);
+                
+            } catch (Exception e) {
+                System.out.println("Error al escribir registros");
+            }
+    }
+
+    static void menuOpciones(){
+        System.out.println( "*********************Bienvenidos al Sistema********************" );
+        
+        char opcion='S';
+        int numeroAlgoritmo=1;
+        do{
+            String datos="Seleccion la opción que desea:\n"+
+
+            "3=Imprimir Productos \n"+
+            "4=Crear Nuevo cliente \n"+
+            "8=Crear Archivo Contenido \n"+
+            "9=Agregar Producto \n"+
+            "10=Eliminar Producto \n"+
+            "11=Modificar Registro de Productos \n"+
+            "12=Registrar Venta \n"+
+            "13=Reporte de Ventas por Dia \n";
+            ProductosDao daoProd;
+            ClientesDao clientesDao;
+            numeroAlgoritmo=teclado.leer(0, datos);
+            switch(numeroAlgoritmo){
+
+                case 2: new MainGUI();break;
+                case 3: rep.imprimirMatriz(rep.reporteDatos());break;
+                case 4: {
+                    clientesDao=new ClientesDao();
+                    clientesDao.crearCliente();
+                    clientesDao.reportarClientes();
+                }break;
+                case 8: crearArchivo(teclado.leer("file.txt", "Ingrese el nombe del archivo que desea crear"));break;
+                case 9: { 
+                    daoProd=new ProductosDao();
+                    daoProd.agregarProducto();
+                    daoProd.reportarProductos();
+                }break;
+                case 10: { 
+                    daoProd=new ProductosDao();                    
+                    daoProd.reportarProductos();
+                    daoProd.eliminarProducto(
+                        teclado.leer("","Ingrese el Id del Producto a Eliminar"));
+                    daoProd.reportarProductos();
+                }break;      
+                case 11: { 
+                    daoProd=new ProductosDao();
+                    daoProd.modificarProducto();
+                    util.clearConsole();
+                    daoProd.reportarProductos();
+                }break;       
+                case 12: { 
+                  VentasDao  vDao=new VentasDao();                    
+                    util.clearConsole();
+                    vDao.registrarVentas();
+                }break;  
+                case 13: { 
+                  VentasDao  vDao=new VentasDao();                    
+                    util.clearConsole();
+                    vDao.reporteVentasFecha(teclado.leer("", "Ingrese la Fecha Inicial"), teclado.leer("", "Ingrese la Fecha Final"));
+                }break;                                                      
+                default : System.out.println("La opción que selecciono no existe"); 
+            }
+            opcion=teclado.leer(' ', "Desea probar otras opciones? SI=S, NO=N");
+            util.clearConsole();
+            System.out.println(datos);
+        }while(opcion=='S' || opcion=='s');
+
+    }
+
+    static boolean login(){
+        String usuario=teclado.leer("", "Ingrese el Usuario:");
+        System.out.println("Ingrese su clave:");
+        char[] clave=cons.readPassword();
+        if(usuario.equals("pollito") && String.valueOf(clave).equals("13579")){
+            menuOpciones();
+            return true;
+        }else{ 
+            System.err.println("Intente Nuevamente ...!!!");
+            login();
+            return false;}        
+    }
+
+
+    public static void main( String[] args ){        
+        
+        util.clearConsole();
+        System.out.println("-----Ingrese su usuario y clave para acceder al Sistema---");
+        login();
+
+    }
+}
---
+++ VentasUpeu/src/main/java/pe/edu/upeu/dao/VentasDao.java
@@ -0,0 +1,148 @@
+package pe.edu.upeu.dao;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import pe.edu.upeu.data.CrudFileRepository;
+import pe.edu.upeu.modelo.Productos;
+import pe.edu.upeu.modelo.VentaDetalle;
+import pe.edu.upeu.modelo.Ventas;
+import pe.edu.upeu.utils.LeerArchivo;
+import pe.edu.upeu.utils.LeerTeclado;
+import pe.edu.upeu.utils.UtilsX;
+
+public class VentasDao extends CrudFileRepository{
+    LeerTeclado leerTecla=new LeerTeclado();
+    UtilsX util=new UtilsX();
+    LeerArchivo leerArc; 
+    Ventas ventasTO;
+    VentaDetalle ventaDetalle;
+
+    SimpleDateFormat formato=new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    SimpleDateFormat formatoFecha=new SimpleDateFormat("dd-MM-yyyy");
+
+    public void reporteVentasFecha(String fInicial, String fFinal){
+        leerArc=new LeerArchivo("Ventas.txt");
+        Object[][] data=listarContenido(leerArc);
+        int cantidadVentas=0;
+        double montoRecaudado=0;
+
+        try {
+            for(int fila=0; fila<data.length; fila++){
+                String[] vectorFecha=data[fila][2].toString().split(" ");
+                Date fechaTemp=formatoFecha.parse(vectorFecha[0].toString());
+                if((fechaTemp.after(formatoFecha.parse(fInicial)) &&  
+                fechaTemp.before(formatoFecha.parse(fFinal))) || vectorFecha[0].equals(fInicial) || vectorFecha[0].equals(fFinal) ){
+                    cantidadVentas++;
+                }
+            }
+
+            Object[][] dataDiaFecha=new Object[cantidadVentas][data[0].length]; 
+            int filaX=0, columnaX=0;
+            for(int fila=0;fila<data.length;fila++){
+                 String[] vectorFecha=data[fila][2].toString().split(" ");
+                 Date fechaTemp=formatoFecha.parse(vectorFecha[0].toString());
+                if((fechaTemp.after(formatoFecha.parse(fInicial)) &&  
+                fechaTemp.before(formatoFecha.parse(fFinal))) || vectorFecha[0].equals(fInicial) || vectorFecha[0].equals(fFinal)){
+                    for(int columna=0; columna<data[0].length;columna++){
+                        dataDiaFecha[filaX][columnaX]=data[fila][columna];
+                        if(columna==3){ montoRecaudado+=Double.parseDouble(data[fila][columna].toString()); }
+                        columnaX++;
+                    }
+                    filaX++;
+                    columnaX=0;
+                }                 
+            }
+            System.out.println("IDVenta\t Cliente\t FechaVenta \t Monto");
+            imprimirLista(dataDiaFecha);
+            System.out.println("***********************Resumen Venta********************");
+            System.out.println("Monto Recaudado: "+montoRecaudado+ " CantidadVentas:"+cantidadVentas);
+
+        } catch (Exception e) {
+            System.out.println("Error en Reporte"+e.getMessage());
+        }
+
+
+    }
+
+    public void registrarVentas(){
+        Ventas ventaX=crearVenta();
+        String continuarVenta="SI";
+        do{
+            util.clearConsole();
+            listarProductosDisponibles();
+            crearCarritoVenta(ventaX);
+            continuarVenta=leerTecla.leer("S", "Desea agregar mas Productos? SI/NO");
+        }while(continuarVenta.toUpperCase().equals("SI") || continuarVenta.toUpperCase().equals("S"));
+
+        actualizarMontoVenta(extraerMontoGeneralVenta(ventaX), ventaX);
+      
+    }
+
+    public double extraerMontoGeneralVenta(Ventas ventaX){
+         leerArc=new LeerArchivo("VentasDetalle.txt");
+         Object[][] data=buscarContenido(leerArc, 1, ventaX.getIdVenta());
+         double monto=0;
+         for(int i=0;i<data.length;i++){
+            monto=monto+Double.parseDouble(data[i][5].toString());
+         }
+        return monto;
+    }
+
+    public Object[][] actualizarMontoVenta(double monto, Ventas ventaX){
+        leerArc=new LeerArchivo("Ventas.txt");
+        ventaX.setMontoVenta(monto);
+        return editarRegistro(leerArc, 0, ventaX.getIdVenta(), ventaX);       
+    }
+
+
+    public Ventas crearVenta(){
+        leerArc=new LeerArchivo("Ventas.txt");
+        ventasTO=new Ventas();
+        ventasTO.setIdVenta(generarId(leerArc, 0, "V", 1));
+        ventasTO.setDniCliente(leerTecla.leer("0", "Ingrese el DNI del cliente:"));
+        ventasTO.setMontoVenta(0);
+        ventasTO.setFechaVenta(formato.format(new Date()));
+        agregarContenido(leerArc, ventasTO);
+        return ventasTO;
+    }
+
+    public void listarProductosDisponibles(){
+       leerArc=new LeerArchivo("Productos.txt"); 
+    System.out.println("******************Productos Disponibles***********");
+    Object[][] data=listarContenido(leerArc);
+    for (int i=0; i<data.length; i++) {
+        System.out.print(data[i][0]+"="+data[i][1]+"(stock:"+data[i][4]+" Precio: "+data[i][3]+" ) | " );
+        }
+    }
+
+    public Object[][] crearCarritoVenta(Ventas venta){            
+      leerArc=new LeerArchivo("VentasDetalle.txt");  
+      ventaDetalle=new VentaDetalle();
+      ventaDetalle.setIdVtnDet(generarId(leerArc, 0, "VD", 2));
+      ventaDetalle.setIdVenta(venta.getIdVenta());
+      String productoId=leerTecla.leer("", "Ingrese el Id del Producto:");
+      ventaDetalle.setiDProducto(productoId);
+      ventaDetalle.setCantidad(leerTecla.leer(0, "Ingrese la cantidad:"));
+      Productos prodTemp=buscarProducto(productoId);
+      ventaDetalle.setPrecioUnit(prodTemp.getPrecio());
+      ventaDetalle.setPrecioTotal(ventaDetalle.getCantidad()*prodTemp.getPrecio());
+      leerArc=new LeerArchivo("VentasDetalle.txt");
+      
+      return agregarContenido(leerArc, ventaDetalle);
+    }
+
+
+    public Productos buscarProducto(String idProducto){
+        leerArc=new LeerArchivo("Productos.txt");  
+       Object[][] data= buscarContenido(leerArc, 0, idProducto);
+       Productos productos=new Productos();
+       if(data!=null){
+        productos.setStock(Double.parseDouble(data[0][4].toString()));
+        productos.setPrecio(Double.parseDouble(data[0][3].toString()));
+       }
+       return productos;
+    }
+
+    
+}
